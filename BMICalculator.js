a9165ac3aaf6e37c4223bb6622fe9b682f63aa03
Orca-63 <sam63mb@gmail.com>
2024-07-27 18:26:29 +0530

    Adding All Files

--- BMICalculator.js
+++ BMICalculator.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (e) {
-    // we will prevent the default function of the form which is to sunbmit the data to some server
+
     console.log(e);
-    // e is the submit event 
+
     e.preventDefault();
 
-    // take the value of height and weight
+
     const height = parseInt(document.querySelector('#height').value);
     console.log(height);
     const weight = parseInt(document.querySelector('#weight').value);
